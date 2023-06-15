9b9f49b89c461ffa7b6fc45b26a0f39375bb4332
Hemandh <hemandh007bond@gmail.com>
2023-06-15 19:18:16 +0530

    fw21_0855_day_3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
-import StudyTimer from "./Components/StudyTimer";
 import MainRoutes from "./Routes/MainRoutes";
 
 function App() {
   return (
     <div className="App">
-      <h1>Study Tracker Homepage</h1>
+      
       <MainRoutes />
-      <StudyTimer/>
+      
     </div>
   );
 }
