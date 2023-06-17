56003482b0b078c2708ee6cc62975f3bb8698851
Hemandh <hemandh007bond@gmail.com>
2023-06-17 14:05:47 +0530

    backend update

--- Backend/Models/todo.model.js
+++ Backend/Models/todo.model.js
@@ -1,1 +1,31 @@
 const mongoose = require("mongoose")
+
+const TodoSchema = mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+const TodoModel = mongoose.model("Todo", TodoSchema);
+
+module.exports = { TodoModel };
